dcb0a0ce94b6b223e0a54d40960463584c3f53e3
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2024-04-25 22:13:44 -0700

    Test

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pickle
 with open('best_model_traditional.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
 
+
 def main(): 
     st.title("Sentiment Predictor")
     html_temp = """
@@ -17,6 +18,20 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
     
     review_combined_lemma = st.text_area('Area for textual entry')
+
     
+    if st.button("Predict"): 
+        features = [[review_combined_lemma]]
+        data = {'review_combined_lemma': str(review_combined_lemma)}
+        #print(data)
+        df=pd.DataFrame([list(data.values())], columns=cols)
+
+        prediction = model.predict(df['review_combined_lemma'].values)
+
+        if prediction == 1:
+            st.success('Positive!!')
+        else:
+            st.success('Negative!!')
+      
 if __name__=='__main__': 
     main()
