8c219acc6cad2d2416dab1e8b80ade5b7ad40375
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2024-04-25 21:40:39 -0700

    streamlit testing

---
+++ posts/deep-learning-project-msis/app.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import streamlit as st 
+import pickle
+import warnings
+
+model = pickle.load(open('best_model_traditional.pkl', 'rb'))
+cols= ['review_combined_lemma']    
+  
+def main(): 
+    st.title("Sentiment Predictor")
+    html_temp = """
+    <div style="background:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Sentiment Prediction App </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html = True)
+    
+    review_combined_lemma = st.text_area('Area for textual entry')
+
+    
+    if st.button("Predict"): 
+        features = [[review_combined_lemma]]
+        data = {'review_combined_lemma': str(review_combined_lemma)}
+        #print(data)
+        df=pd.DataFrame([list(data.values())], columns=cols)
+
+        prediction = model.predict(df['review_combined_lemma'].values)
+
+        if prediction == 1:
+            st.success('Positive!!')
+        else:
+            st.success('Negative!!')
+      
+if __name__=='__main__': 
+    main()
