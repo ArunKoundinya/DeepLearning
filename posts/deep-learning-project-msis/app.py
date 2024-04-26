a8e9011760827513f0452d2137f3e11016bfe1e5
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2024-04-25 21:51:48 -0700

    test

--- posts/deep-learning-project-msis/app.py
+++ posts/deep-learning-project-msis/app.py
@@ -2,11 +2,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st 
 import pickle
-import warnings
 
-model = pickle.load(open('posts/deep-learning-project-msis/best_model_traditional.pkl', 'rb'))
-cols= ['review_combined_lemma']    
-  
+
+
 def main(): 
     st.title("Sentiment Predictor")
     html_temp = """
@@ -17,20 +15,6 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
     
     review_combined_lemma = st.text_area('Area for textual entry')
-
     
-    if st.button("Predict"): 
-        features = [[review_combined_lemma]]
-        data = {'review_combined_lemma': str(review_combined_lemma)}
-        #print(data)
-        df=pd.DataFrame([list(data.values())], columns=cols)
-
-        prediction = model.predict(df['review_combined_lemma'].values)
-
-        if prediction == 1:
-            st.success('Positive!!')
-        else:
-            st.success('Negative!!')
-      
 if __name__=='__main__': 
     main()
